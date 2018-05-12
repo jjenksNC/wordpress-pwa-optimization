d9685814e9d00e8ff8841adaca31d73d48a8ac50
o10n-x <info@optimization.team>
2018-05-12 23:57:20 +0200

    core update

--- pwa-optimization.php
+++ pwa-optimization.php
@@ -12,7 +12,7 @@ namespace O10n;
  * @wordpress-plugin
  * Plugin Name:       PWA Optimization
  * Description:       Advanced Progressive Web App optimization toolkit. Service Worker, HTML fragment streaming, HTTP/2 Cache-Digest calculation, Web App Manifest editor and more.
- * Version:           0.0.20
+ * Version:           0.0.21
  * Author:            Optimization.Team
  * Author URI:        https://optimization.team/
  * GitHub Plugin URI: https://github.com/o10n-x/wordpress-pwa-optimization
@@ -30,7 +30,7 @@ if (defined('WP_INSTALLING') && WP_INSTALLING) {
 }
 
 // settings
-$module_version = '0.0.20';
+$module_version = '0.0.21';
 $minimum_core_version = '0.0.44';
 $plugin_path = dirname(__FILE__);
 
